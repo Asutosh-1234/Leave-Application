38ae8f253b78d75e3fa0c94ab61746dd9f99f615
Asutosh-1234 <pandaashutosh220@gmail.com>
2026-05-21 18:08:24 +0530

    feat(leave): add applyLeave controller for leave application

---
+++ src/controller/leave.controller.js
@@ -0,0 +1,13 @@
+import prisma from "../utilities/prisma-client.js";
+import { ApiResponse } from "../utilities/api-response.js";
+import { ApiError } from "../utilities/api-error.js";
+import { asyncHandler } from "../utilities/async-handler.js";
+
+const applyLeave = asyncHandler(async (req, res)=>{
+  const {reason, details} = req.body;
+
+  if(!reason || !details){
+    throw new ApiError(400, "reason or details is missing");
+  }
+  
+})
